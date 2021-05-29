088aeb87ea131e6b0756020bf1254ea86cf22781
faridagil <78220195+faridagil@users.noreply.github.com>
2021-05-30 01:48:32 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+
+  <div>
+
+    <div>Selamat Datang</div>
+
+    <ul>
+
+      <li v-for="item in todos">{{ item.desc }}</li>
+
+    </ul>
+
+    <input v-model="myText"/>
+
+    <button @click="tambah">Add</button>
+
+  </div>
+
+</template>
+
+
+
+<script>
+
+export default{
+
+  data: function () {
+
+    return{
+
+      todos: [
+
+        { desc: 'Makan Durian'},
+
+        { desc: 'Makan Tempe '},
+
+        { desc: 'Coding JS'}
+
+      ],
+
+      myText: ''
+
+    }
+
+  },
+
+
+
+  methods: {
+
+    tambah: function () {
+
+      this.todos.push({desc: this.myText})
+
+    }
+
+  }
+
+}
+
+</script>
